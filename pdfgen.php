97b3119cdb8e3bca547128eff776133b94736ab6
Nahuel Angelinetti <nahuel@ahtna.org>
2020-09-29 17:05:20 +0200

    Intégration de la génération du PDF avec gotenberg dans le plugin

--- pdfgen.php
+++ pdfgen.php
@@ -1,4 +1,16 @@
 <?php
+
+require_once 'share/plugins/custom/pdfgen/vendor/autoload.php';
+
+use TheCodingMachine\Gotenberg\Client;
+use TheCodingMachine\Gotenberg\ClientException;
+use TheCodingMachine\Gotenberg\DocumentFactory;
+use TheCodingMachine\Gotenberg\HTMLRequest;
+use TheCodingMachine\Gotenberg\URLRequest;
+use TheCodingMachine\Gotenberg\Request;
+use TheCodingMachine\Gotenberg\RequestException;
+use GuzzleHttp\Psr7\LazyOpenStream;
+
 class pdfgen extends Plugins {
 	public function enableAction (&$context, &$error) {
 		if(!parent::_checkRights(LEVEL_ADMINLODEL)) { return; }
@@ -21,5 +33,41 @@ class pdfgen extends Plugins {
 		View::getView()->renderCached('pdfgen');
 		return "_ajax";
 	}
+
+	public function getAction (&$context, &$error) {
+		setlocale(LC_ALL, 'C');
+
+		$document = $context['document'];
+		$lang = isset($context['lang']) ? $context['lang'] : "";
+		$cache_path = getcwd() . DIRECTORY_SEPARATOR . 'CACHE';
+
+		if ( ! is_dir($cache_path) ) {
+			mkdir($cache_path, 0755, TRUE);
+                }
+		$article_url = "${context['siteurl']}/?do=_pdfgen_view&document=${document}&lang=${lang}";
+
+		$cache_key = md5($article_url);
+                $cache_file = $cache_path . DIRECTORY_SEPARATOR . $cache_key;
+
+		if ( ! file_exists( $cache_file ) || filemtime( $cache_file ) < time()) {
+
+			$client = new Client($this->_config['gotenberg_url']['value']);
+			$request = new URLRequest($article_url);
+
+			$request->setPaperSize(Request::A4);
+			$request->setMargins(Request::NO_MARGINS);
+
+			$client->store($request, $cache_file);
+			$client->post($request);
+
+		}
+
+		header('Content-Type: application/pdf');
+		header('Content-Disposition: attachment; filename="downloaded.pdf"');
+		echo file_get_contents($cache_file);
+
+		return "_ajax";
+	}
+
 }
 ?>
